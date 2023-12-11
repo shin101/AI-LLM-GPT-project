73c07b68d6e04dd87d71c2fdfc6fff8ab7585ce0
shin101 <kendras0127@gmail.com>
2023-12-11 17:14:14 -0500

    Quesitons Prompt

--- pages/02_QuizGPT.py
+++ pages/02_QuizGPT.py
@@ -2,6 +2,8 @@ import streamlit as st
 from langchain.retrievers import WikipediaRetriever
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
 
 st.set_page_config(
@@ -12,7 +14,9 @@ st.title("QuizGPT")
 
 llm = ChatOpenAI(
     temperature=0.1,
-    model="gpt-3.5-turbo-1106"
+    model="gpt-3.5-turbo-1106",
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()]
 )
 
 @st.cache_data(show_spinner="Loading file...")
@@ -29,6 +33,10 @@ def split_file(file):
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     return docs
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
 
 with st.sidebar:
     docs = None
@@ -56,4 +64,46 @@ if not docs:
     """
     )
 else:
-    st.write(docs)
+    prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+        """
+            You are a helpful assistant that is role playing as a teacher.
+                
+            Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+            
+            Each question should have 4 answers, three of them must be incorrect and one should be correct.
+                
+            Use (o) to signal the correct answer.
+                
+            Question examples:
+                
+            Question: What is the color of the ocean?
+            Answers: Red|Yellow|Green|Blue(o)
+                
+            Question: What is the capital or Georgia?
+            Answers: Baku|Tbilisi(o)|Manila|Beirut
+                
+            Question: When was Avatar released?
+            Answers: 2007|2001|2009(o)|1998
+                
+            Question: Who was Julius Caesar?
+            Answers: A Roman Emperor(o)|Painter|Actor|Model
+                
+            Your turn!
+                
+            Context: {context}
+        """,
+                    )
+                ]
+            )
+
+    chain = {
+        "context": format_docs
+            } | prompt | llm
+
+    start = st.button("Generate Quiz")
+
+    if start:
+        chain.invoke(docs)
